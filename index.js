c5126e5c1006fda683fae10b8b98ee4ee45b153e
Ryan Ardray <rardray@gmail.com>
2019-03-17 17:15:00 -0500

    l

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const path = require("path");
 mongoose.connect(config.database, { useNewUrlParser: true });
 app.use("/public", express.static(__dirname + "/public"));
 app.use(express.static(path.join(__dirname, "client/build")));
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
