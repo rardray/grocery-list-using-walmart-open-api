4390fd4d7a455ccfdfa1d126540ddb64920fb3e0
Ryan Ardray <rardray@gmail.com>
2019-03-17 20:05:45 -0500

    j

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, "client/build")));
 app.get("/", function(req, res) {
   res.sendFile(path.resolve(__dirname + "/client/build/index.html"));
 });
-app.get("/*", function(req, res) {
+app.get("*", function(req, res) {
   res.sendFile(path.resolve(__dirname + "/client/build/index.html"));
 });
 app.use(logger("dev"));
