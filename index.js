2c84525abf1c41582d69b04e40a34ac581d3384f
Ryan Ardray <rardray@gmail.com>
2019-03-17 21:36:12 -0500

    e

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ console.log("Server running on " + config.port);
 const path = require("path");
 
 mongoose.connect(config.database, { useNewUrlParser: true });
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use("/", express.static(path.join(__dirname, "client/build")));
 
 app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
