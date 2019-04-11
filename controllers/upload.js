2c570e45edf1ac0bc387eacebacce7aeddfc8c5c
Ryan Ardray <rardray@gmail.com>
2019-04-11 01:54:56 -0500

    kk

--- controllers/upload.js
+++ controllers/upload.js
@@ -3,7 +3,9 @@ const singleUpload = upload.single("image");
 
 exports.postImage = function(req, res, next) {
   singleUpload(req, res, function(err, some) {
-    if (err) next(err);
+    if (err) {
+      res.status(422).send("oh no");
+    }
     res.json({ imageUrl: req.file.location });
   });
 };
