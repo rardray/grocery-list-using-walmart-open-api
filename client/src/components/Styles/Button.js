15d3edb907145eed0100755c21a36ea13227c288
Ryan Ardray <rardray@gmail.com>
2019-04-11 16:17:05 -0500

    removing a few frivilous animations, fixing some margin issues

--- client/src/components/Styles/Button.js
+++ client/src/components/Styles/Button.js
@@ -1,34 +1,13 @@
 import React, { useState } from "react";
 import "./main.css";
-import styled, { keyframes, css } from "styled-components";
-
-const highlight = keyframes`
-  0% { background: black; }
-  100% { background: grey; }
-  `;
-const animation = props =>
-  css`
-    ${highlight} .3s ease-in-out 1;
-  `;
 
 export default function Button(props) {
-  const [hover, setHover] = useState(false);
   const { label, click } = props;
-  const Btn = styled.button`
-    background: ${hover ? "grey" : "slategrey"};
-    color: white;
-    animation: ${hover ? animation : "none"};
-  `;
   return (
     <div>
-      <Btn
-        style={{ zIndex: 15 }}
-        onMouseLeave={() => setHover(!hover)}
-        onMouseEnter={() => setHover(!hover)}
-        onClick={click}
-      >
+      <button style={{ zIndex: 15 }} onClick={click}>
         {label}
-      </Btn>
+      </button>
     </div>
   );
 }
