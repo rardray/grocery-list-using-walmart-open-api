8b839db9176c9861db2ac7ee9406d33a68b96463
Ryan Ardray <rardray@gmail.com>
2019-05-05 03:56:46 -0500

    mod - a mess

--- client/src/components/Home/MealForDay.js
+++ client/src/components/Home/MealForDay.js
@@ -1,4 +1,8 @@
 import React from "react";
+import plus from "../Styles/plus.svg";
+import edit from "../Styles/edit.svg";
+import { Link } from "@reach/router";
+import go from "../Styles/go.svg";
 
 export default function MealForDay(props) {
   const today = props.meals.filter(el => {
@@ -12,10 +16,107 @@ export default function MealForDay(props) {
     <div style={{ textAlign: "center" }}>
       <div className="food-today">
         {today[0] ? <img src={today[0].mainId.image} alt="meal" /> : null}
-        <h4>What's For Dinner:</h4>
-        <h4>
+        <h3>What's For Dinner...</h3>
+        <h3>
           {today[0] ? today[0].mainId.title : "Nothing planned for today."}
-        </h4>
+        </h3>
+        <div
+          style={{
+            display: "inline-block",
+            position: "absolute",
+            bottom: 4,
+            left: 0,
+            width: "98%",
+            textAlign: "center",
+            padding: 0,
+            paddingTop: 6,
+            borderTop: "1px solid lightslategrey"
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexFlow: "row wrap",
+              justifyContent: "space-evenly"
+            }}
+          >
+            {today[0] ? (
+              <div
+                style={{
+                  display: "inline-block",
+                  position: "relative",
+                  marginLeft: 10,
+                  marginRight: 10
+                }}
+              >
+                <Link to="/recipe/caldata/edit">
+                  <img
+                    src={edit}
+                    alt="edit"
+                    style={{ width: 25, height: 25, margin: 0 }}
+                  />
+                  <br />
+                  edit meal
+                </Link>
+              </div>
+            ) : (
+              <div
+                style={{
+                  display: "inline-block",
+                  position: "relative",
+                  marginLeft: 10,
+                  marginRight: 10
+                }}
+              >
+                <Link to="/grocery/addplan/:mo/:ye/:dy">
+                  <img
+                    src={plus}
+                    alt="add"
+                    style={{ width: 25, height: 25, margin: 0 }}
+                  />
+                  <br />
+                  add meal
+                </Link>
+              </div>
+            )}
+            <div
+              style={{
+                display: "inline-block",
+                position: "relative",
+                marginLeft: 10,
+                marginRight: 10
+              }}
+            >
+              <Link to="/grocery/addrecipe">
+                <img
+                  src={plus}
+                  alt="add"
+                  style={{ width: 25, height: 25, margin: 0 }}
+                />
+                <br />
+                new recipe
+              </Link>
+            </div>
+            <div
+              style={{
+                display: "inline-block",
+                position: "relative",
+                marginLeft: 10,
+                marginRight: 10
+              }}
+            >
+              <Link to="/grocery/recipe">
+                <img
+                  src={go}
+                  alt="add"
+                  style={{ width: 25, height: 25, margin: 0, borderRadius: 0 }}
+                />
+                <br />
+                view meal
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
